50ed5b4c08ac47af4c00781202f7e1fabaa582fe
Daiki Obata <d.obata@diamondhead.tech>
2024-05-29 18:52:56 +0900

    https://www.youtube.com/watch?v=WyGN5CT9iCs&list=PLB4AdipoHpxZu39dNjUWGRwgeKgW7mVJ-&index=13&pp=iAQB による変更

--- app/routes/app.settings.jsx
+++ app/routes/app.settings.jsx
@@ -1,21 +1,37 @@
 import {
   Box,
   Card,
-  Divider,
   InlineGrid,
-  Layout,
-  Link,
-  List,
   Page,
   Text,
   TextField,
   BlockStack,
-  useBreakpoints,
+  Button,
 } from "@shopify/polaris";
 import { TitleBar } from "@shopify/app-bridge-react";
+import { useState } from "react";
+import { json } from "@remix-run/node";
+import { useLoaderData, Form } from "@remix-run/react";
+
+
+export async function loader() {
+  // get data from database
+  const settings = {
+    name: "My app updated",
+    description: "My app description",
+  };
+  return json(settings);
+}
+
+export async function action({ request }) {
+  let settings = await request.formData()
+  settings = Object.fromEntries(settings);
+  return json(settings);
+}
 
 export default function SettingsPage() {
-  const { smUp } = useBreakpoints();
+  const settings = useLoaderData();
+  const [formState, setFormState] = useState(settings);
 
   return (
     <Page>
@@ -29,41 +45,21 @@ export default function SettingsPage() {
           >
             <BlockStack gap="400">
               <Text as="h3" variant="headingMd">
-                InterJambs
+                Settings
               </Text>
               <Text as="p" variant="bodyMd">
-                Interjambs are the rounded protruding bits of your puzzlie piece
+                Update app settings and preferences.
               </Text>
             </BlockStack>
           </Box>
           <Card roundedAbove="sm">
-            <BlockStack gap="400">
-              <TextField label="Interjamb style" />
-              <TextField label="Interjamb ratio" />
-            </BlockStack>
-          </Card>
-        </InlineGrid>
-        {smUp ? <Divider /> : null}
-        <InlineGrid columns={{ xs: "1fr", md: "2fr 5fr" }} gap="400">
-          <Box
-            as="section"
-            paddingInlineStart={{ xs: 400, sm: 0 }}
-            paddingInlineEnd={{ xs: 400, sm: 0 }}
-          >
-            <BlockStack gap="400">
-              <Text as="h3" variant="headingMd">
-                Dimensions
-              </Text>
-              <Text as="p" variant="bodyMd">
-                Interjambs are the rounded protruding bits of your puzzlie piece
-              </Text>
-            </BlockStack>
-          </Box>
-          <Card roundedAbove="sm">
-            <BlockStack gap="400">
-              <TextField label="Horizontal" />
-              <TextField label="Interjamb ratio" />
-            </BlockStack>
+            <Form method="POST">
+              <BlockStack gap="400">
+                <TextField label="App name" name="name" value={formState.name} onChange={(value) => setFormState({ ...formState, name: value })} />
+                <TextField label="Description" name="description" value={formState.description} onChange={(value) => setFormState({ ...formState, description: value })} />
+                <Button submit={true}>Save </Button>
+              </BlockStack>
+            </Form>
           </Card>
         </InlineGrid>
       </BlockStack>
